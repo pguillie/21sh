e26074e1ee7d76a6ba8a15a13695b911149054d2
pguillie <pguillie@student.42.fr>
2017-09-07 14:12:11 +0200

    update libft

--- libft/src/libft/ft_strappend.c
+++ libft/src/libft/ft_strappend.c
@@ -6,20 +6,27 @@
 /*   By: pguillie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 18:46:21 by pguillie          #+#    #+#             */
-/*   Updated: 2017/08/31 11:15:43 by pguillie         ###   ########.fr       */
+/*   Updated: 2017/09/07 14:10:27 by pguillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strappend(char *base, char const *to_app, char side)
+char    *ft_strappend(char *str, const char *content)
 {
-	char	*new;
+	char    *new;
+	size_t  i[2];
 
-	if (side == 'l')
-		new = ft_strjoin(to_app, base);
-	if (side == 'r')
-		new = ft_strjoin(base, to_app);
-	base ? ft_strdel(&base) : 0;
+	if ((new = ft_strnew(ft_strlen(str) + ft_strlen(content))))
+	{   
+		i[0] = 0;
+		if (str && !(i[1] = 0))
+			while (str[i[1]])
+				new[i[0]++] = str[i[1]++];
+		if (content && !(i[1] = 0))
+			while (content[i[1]])
+				new[i[0]++] = content[i[1]++];
+	}
+	str ? ft_strdel(&str) : 0;
 	return (new);
 }
