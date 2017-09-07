a876756a83d5dcd89517dbf4b20a7cf6cf299e54
pguillie <pguillie@student.42.fr>
2017-09-07 14:06:10 +0200

    DONE: secu hist_able | NEW: escaped '\n'

--- src/hci/edition/sh_hist_able.c
+++ src/hci/edition/sh_hist_able.c
@@ -3,10 +3,10 @@
 static int	sh_convert_hist(char *esc)
 {
 	if (esc[1] == '[' && (esc[2] == 'A' || esc[2] == 'B'))
-		return (1 << ((esc[2] ^ 64) - 1));
+		return (esc[2] ^ 64);
 	else if (esc[1] == '[' && (esc[2] == '5' || esc[2] == '6') && esc[3] == '~')
-		return (2 << (~esc[2] ^ 8));
-return (0);
+		return (esc[2] ^ 52);
+	return (0);
 }
 
 t_line		*sh_hist_able(char *esc, t_line *line)
@@ -14,12 +14,11 @@ t_line		*sh_hist_able(char *esc, t_line *line)
 	t_line	*target;
 	int		id;
 
+	target = NULL;
 	id = sh_convert_hist(esc);
-	if ((id & UP || id & P_UP) && line->up)
+	if (id & UP && line->up)
 		target = line->up;
-	else if ((id & DOWN || id & P_DO) && line->down)
+	else if (id & DOWN && line->down)
 		target = line->down;
-	else
-		return (line);
 	return (target);
 }
