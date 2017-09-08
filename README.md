008ec8d8972be1eceb05432a9f56aefeb5d46034
mdescamp <mdescamp@student.42.fr>
2017-09-08 16:59:58 +0200

    done: sh_syntax

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ melanger too big + \n
 
 PROMPT:
 prompt_len est la longueur du prompt depuis le DERNIER '\n' (s'il existe)
-
-lexer error:
-$ &> toto ls // insinue que "&>" = ESPERCOLON
-$ <& toto ls // zsh demande un fd
--- src/parser/sh_syntax.c
+++ src/parser/sh_syntax.c
@@ -2,7 +2,7 @@
 
 int		sh_syntax(t_token *lexer)
 {
-	if (lexer->category != CMD && lexer->category != ESPERCOLON
+	if (lexer->category != CMD && !ft_strequ(lexer->lexeme, ";")
 			&& lexer->category != FILDES && lexer->category != REDIRECTION)
 		return (ft_error("sla_sh", "syntax error near unexpected token"
 					, lexer->lexeme));
