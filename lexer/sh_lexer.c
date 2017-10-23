778889b6a5ea672a912e163072a27a2f0cd7a021
Ludovic CORDIER <lcordier@e1r10p15.42.fr>
2017-10-23 18:51:43 +0200

    lexer avant test

--- lexer/sh_lexer.c
+++ lexer/sh_lexer.c
@@ -5,18 +5,19 @@ int		sh_lexer(t_token **list, char *str)
 	char	*eof_fifo[32];
 	char	*lexeme;
 	int		category;
-	int		status[2];
+	int		status[3];
 	int		i[2];
 
 	i[0] = 0;
-	status[0] = COMMAND;
+	status[0] = CMD;
 	status[1] = 0;
+	status[2] = 0;
 	while (str[i[0]])
 	{
 		i[1] = 0;
-		if (status[1] = 42 && eof_fifo[0])
+/*		if (status[2] = 1 && eof_fifo[0])
 			sh_lex_heredoc(str, i, eof_fifo, list);
-		else if (!sh_metachar(str[i[0]]))
+		else */if (!sh_metachar(str[i[0]]))
 			i[1] = sh_lex_word(str + i[0]);
 		else if (!(i[1] = sh_rdir_op(str + i[0]))
 				&& !(i[1] = sh_ctrl_op(str + i[0])))
@@ -24,15 +25,11 @@ int		sh_lexer(t_token **list, char *str)
 		if (i[1])
 		{
 			lexeme = ft_strsub(str, i[0], i[1]);//
-			category = sh_category(lexeme, str + i[0] + i[1], status);
-			if (category == NEWLINE)
-				status[1] = 42;
-			else if (category > 0)
-				status[0] = COMMAND;
-			else if (category == REDIR)
-				status[1] = 1;
+			category = sh_category(str, i, status);
+			status[1] = category == REDIRECTION ? 1 : 0;
 			sh_token_new(list, lexeme, category);//
 		}
-		i[0]++;
+		i[0] += i[1] + 1;
 	}
+	return (0);
 }
