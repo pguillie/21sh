cc9e5baba5a529fd07c58175d613e45a53a6e37e
pguillie <pguillie@student.42.fr>
2017-10-23 19:10:30 +0200

    heredoc 1st

--- lexer/sh_lexer.c
+++ lexer/sh_lexer.c
@@ -9,15 +9,16 @@ int		sh_lexer(t_token **list, char *str)
 	int		i[2];
 
 	i[0] = 0;
+	ft_memset(eof_fifo, 0, sizeof(char*) * 32);
 	status[0] = CMD;
 	status[1] = 0;
 	status[2] = 0;
 	while (str[i[0]])
 	{
 		i[1] = 0;
-/*		if (status[2] = 1 && eof_fifo[0])
+		if (status[2] = 1 && eof_fifo[0])
 			sh_lex_heredoc(str, i, eof_fifo, list);
-		else */if (!sh_metachar(str[i[0]]))
+		else if (!sh_metachar(str[i[0]]))
 			i[1] = sh_lex_word(str + i[0]);
 		else if (!(i[1] = sh_rdir_op(str + i[0]))
 				&& !(i[1] = sh_ctrl_op(str + i[0])))
