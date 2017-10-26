1ccee6ed62528cbd808bafc95265e1d5b938e50b
Ludovic CORDIER <lcordier@student.42.fr>
2017-10-26 15:52:31 +0200

    "COMP: +init"

--- src/hci/completion/sh_completion.c
+++ src/hci/completion/sh_completion.c
@@ -1,20 +1,26 @@
 #include "shell.h"
 
-static int	sh_cmp_init(char *str, size_t cur, char **lexeme, int status)
+static int	sh_cmp_init(char *str, size_t cur, char **lexeme, int *status)
 {
 	size_t	i[2];
+	int		status;
 
 	ft_bzero(i);
-	while (str[i[0]])
+	while (i[0] < cur)
 	{
-		while (str[i[0] + i[1]] && !sh_metachar(str[i[0] + i[1]]))
+		while (i[0] < cur && str[i[0] + i[1]] && !sh_metachar(str[i[0] + i[1]]))
 			i[1]++;
-		if ()
-		
+		if (i[0] + i[1] == cur)
+		{
+			if (!(*lexeme = ft_strsub(str, i[0], i[1])))
+				return (-1);
+			return (0);
+		}
+		if (sh_rdir_op(str + i[0] + i[1]))
+			*status = ARG;
+		else if (sh_ctrl_op(str + i[0] + i[1]))
+			*status = CMD;
+		i[0] += i[1];
 	}
-}
-
-int			sh_completion(t_line *line)
-{
-	
+	return (0);
 }
