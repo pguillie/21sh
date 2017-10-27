a97c6b19edebdd8deb5319303ee503429dca053b
pguillie <pguillie@student.42.fr>
2017-10-27 16:57:43 +0200

    revert push fb71b214f14f6121e7524b8ed01afbd03dcce066

--- src/execution/sh_tree_browse.c
+++ src/execution/sh_tree_browse.c
@@ -40,8 +40,8 @@ static int	sh_pipe(t_tree *root, int ret)
 	}
 	else
 	{
-		waitpid(child, &ret, 0);
 		close(fildes[1]);
+		waitpid(child, &ret, 0);
 		ret = sh_pipe_right(root, fildes[0], ret);
 	}
 	return (ret);
@@ -52,7 +52,7 @@ static int	sh_tree_cmd(t_tree *root, int ret)
 	extern char	**environ;
 	int			fd[10];
 	int			std[3];
-	int			red_ret = 0;
+	int			red_ret;
 
 	memset(fd, -1, sizeof(int) * 10);
 	sh_redir_backup(std);
@@ -60,7 +60,6 @@ static int	sh_tree_cmd(t_tree *root, int ret)
 		ret = sh_execution(root->cmd->av, environ, ret);
 	sh_redir_restore(fd, std);
 	return (red_ret ? red_ret : ret);
-	return (ret);
 }
 
 int			sh_tree_browse(t_tree *root, int ret)
@@ -69,7 +68,7 @@ int			sh_tree_browse(t_tree *root, int ret)
 	{
 		if (root->cmd)
 			ret = sh_tree_cmd(root, ret);
-		if (root->op)
+		else if (root->op)
 		{
 			if (ft_strequ(root->op, "|"))
 				ret = sh_pipe(root, ret);
