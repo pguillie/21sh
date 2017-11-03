c2fc68bdfe8f3fb4cac86390cfd36a2c710166cf
pguillie <pguillie@student.42.fr>
2017-11-03 18:39:38 +0100

    coloration syntaxique OK

--- includes/sh_color.h
+++ includes/sh_color.h
@@ -1,11 +1,15 @@
 #ifndef SH_COLOR_H
 # define SH_COLOR_H
 
-# define RED 	"\033[31m"
-# define WHITE 	"\033[37m"
-# define BLUE 	"\033[34m"
-# define GREEN 	"\033[32m"
-# define CYAN 	"\033[36m"
-# define EOC 	"\033[0m"
+# define C_PIPE		"\e[30;1m"
+# define C_SEPAR	"\e[37;1m"
+# define C_REDIR	"\e[34;1m"
+# define C_CMD		"\e[31;1m"
+# define C_OPT		"\e[35m"
+# define C_ARG		"\e[33m"
+# define C_FILDES	"\e[36m"
+# define C_HEREDOC	"\e[32m"
+# define C_HDMATCH	"\e[32;1m"
+# define C_EOC		"\e[0m"
 
 #endif
--- src/hci/edition/sh_display.c
+++ src/hci/edition/sh_display.c
@@ -1,86 +1,37 @@
 #include "shell.h"
 
-static void	sh_nl_correct(t_line *line, size_t i, t_coord *coord, t_tc tc)
+static void	sh_disp_nl(t_line *line, t_coord *coord, t_tc tc)
 {
-	if (line->str[i - 1] != '\n' && line->cur != i
-			&& coord[i - 1].x == coord[line->used + 1].x - 1)
+	if (line->used && line->str[line->used - 1] != '\n'
+			&& line->cur != line->used
+			&& coord[line->used - 1].x == coord[line->used + 1].x - 1)
 	{
 		ft_putchar_fd(' ', 0);
 		tputs(tc.le, 0, termput);
 	}
 }
 
-static int	sh_display_line(t_line *line, size_t i)
-{
-	while (line->str[i])
-		ft_putchar_fd(line->str[i++], 0);
-	return (i);
-}
-
-static char	*sh_color_theme(int category)
-{
-	static char	*theme[10] = {
-		"\e[32m",
-		"\e[36m",
-		"\e[33m",
-		"\e[35m",
-		"\e[31;1m",
-		"\e[34;1m",
-		"\e[30;1m",
-		"",
-		"\e[37;1m",
-		"\e[37;1m"
-	};
-
-	return (theme[category + 5]);
-}
-
-static int	sh_display_category(t_line *line, int i)
-{
-	int		j;
-	t_token	*lexer;
-	t_token	*tmp;
-
-	lexer = NULL;
-	if (sh_lexer(&lexer, line->str) < 0)
-		return (sh_token_del(&lexer));
-	tmp = lexer;
-	while (lexer)
-	{
-		j = 0;
-		if (lexer->category == HEREDOC)
-			lexer = lexer->next;
-		ft_putstr_fd(sh_color_theme(HEREDOC), 0);
-		while (!(ft_strnequ(line->str + i, lexer->lexeme,
-						ft_strlen(lexer->lexeme))))
-			ft_putchar_fd(line->str[i++], 0);
-		ft_putstr_fd(sh_color_theme(lexer->category), 0);
-		while (lexer->lexeme[j++])
-			ft_putchar_fd(line->str[i++], 0);
-		ft_putstr_fd(EOC, 0);
-		lexer = lexer->next;
-	}
-	sh_token_del(&tmp);
-	return (i);
-}
-
 int			sh_display(t_line *line, t_coord **coord, t_tc tc, char *save)
 {
-	int		i;
+	t_token	*lexer;
+	char	*syntax;
 
-	line->cur = sh_move_cur(line->cur, 0, *coord, tc);
-	if (sh_clear(line, coord, tc) < 0)
-		return (-1);
-	if (!line->used)
-		return (0);
-	i = 0;
-	if (!save)
-		i = sh_display_category(line, i);
+	syntax = getenv("SYNTAX");
+	lexer = NULL;
+	if (!syntax || !ft_strequ(syntax, "ON") || save)
+	{
+		if (sh_clear(line, coord, tc) < 0)
+			return (-1);
+		ft_putstr_fd(line->str + line->cur, 0);
+	}
 	else
-		i = sh_display_line(line, i);
-	if (i < 0)
-		return (-1);
-	sh_nl_correct(line, i, *coord, tc);
-	line->cur = sh_move_cur(i, line->pos, *coord, tc);
+	{
+		line->cur = sh_move_cur(line->cur, 0, *coord, tc);
+		if (sh_clear(line, coord, tc) < 0)
+			return (-1);
+		sh_display_syntax(line->str);
+	}
+	sh_disp_nl(line, *coord, tc);
+	line->cur = sh_move_cur(line->used, line->pos, *coord, tc);
 	return (0);
 }
