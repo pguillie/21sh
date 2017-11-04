4fb3e74b8d997b41d39fdc2a4b21963e97915eb5
Ludovic CORDIER <lcordier@student.42.fr>
2017-11-04 18:50:29 +0100

    edit display

--- includes/sh_hci.h
+++ includes/sh_hci.h
@@ -67,7 +67,7 @@ int		sh_esc(t_line **line, t_coord **coord, t_tc *tc, int *h_smd);
 int		sh_putesc(t_line **line, t_coord **coord, t_tc *tc, int *h_smd);
 int		sh_clear(t_line *line, t_coord **coord, t_tc tc);
 int		sh_display(t_line *line, t_coord **coord, t_tc tc, char *save);
-void	sh_display_syntax(char *str);
+int		sh_display_syntax(char *str);
 int		sh_del_l(t_line *line, t_coord **coord, t_tc tc);
 int		sh_del_r(t_line *line);
 int		sh_hist(t_line **line, t_coord **coord, t_tc tc, t_line *target);
